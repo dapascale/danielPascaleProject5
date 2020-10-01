956b98afe5cd094fe7b23955d6737a01f304fa87
Daniel <dapascale@gmail.com>
2020-09-30 22:10:28 -0400

    Updates

---
+++ precache-manifest.222e2234e6618436b1a076b951ff367b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7bbc0c06093f9cbdc543b00cb80b9c",
+    "url": "/danielPascaleProject5/index.html"
+  },
+  {
+    "revision": "02e351cbd0d354758a60",
+    "url": "/danielPascaleProject5/static/css/main.53c574f5.chunk.css"
+  },
+  {
+    "revision": "13f08ebd9ec7e6b40be0",
+    "url": "/danielPascaleProject5/static/js/2.33df785f.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/danielPascaleProject5/static/js/2.33df785f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e351cbd0d354758a60",
+    "url": "/danielPascaleProject5/static/js/main.b26325b2.chunk.js"
+  },
+  {
+    "revision": "093193c9f7382d6bcff5",
+    "url": "/danielPascaleProject5/static/js/runtime-main.5ac66d81.js"
+  },
+  {
+    "revision": "80cfeef29f38f414c6aa2b17cdf3da2a",
+    "url": "/danielPascaleProject5/static/media/nineties2.80cfeef2.gif"
+  },
+  {
+    "revision": "0ff3b586a7c0a0ae9eed8a1f87660d0a",
+    "url": "/danielPascaleProject5/static/media/uptagico.0ff3b586.png"
+  }
+]);
